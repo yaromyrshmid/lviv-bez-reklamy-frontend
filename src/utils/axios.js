2ffeb4f810a7f57a0b509159dff9be35954b7871
yaromyrshmid <yaromyr.shmid@gmail.com>
2020-01-02 08:30:27 +0200

    Changed axios back to https.

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
-      ? "wss://lviv-bez-reklamy-server.herokuapp.com/"
+      ? "http://lviv-bez-reklamy-server.herokuapp.com/"
       : "http://localhost:5000/"
 });
 
