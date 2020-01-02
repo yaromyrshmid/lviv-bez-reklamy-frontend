b68d03139512764e87d7187936561af0b02edc4f
yaromyrshmid <yaromyr.shmid@gmail.com>
2020-01-02 08:24:22 +0200

    Changed http to wss in axios

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
-      ? "https://lviv-bez-reklamy-server.herokuapp.com/"
+      ? "wss://lviv-bez-reklamy-server.herokuapp.com/"
       : "http://localhost:5000/"
 });
 
