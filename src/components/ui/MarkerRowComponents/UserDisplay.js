64b308947863519fb5d34a1ee216ad30de5217d5
yaromyrshmid <yaromyr.shmid@gmail.com>
2020-01-10 11:05:02 +0200

    Fixed displaying markers from non-existing users

--- src/components/ui/MarkerRowComponents/UserDisplay.js
+++ src/components/ui/MarkerRowComponents/UserDisplay.js
@@ -5,24 +5,30 @@ import { Button } from "../";
 const UserDisplay = ({ user, banUser }) => {
   return (
     <UserDisplayWrapper>
-      <h5>{user.name}</h5>
-      <ManageUserContainer>
-        <span className="user-data">
-          {user.email}
-          <br />
-          Роль: {user.role} <br />
-          ID: {user._id}
-        </span>
-        <div>
-          <Button
-            danger
-            onClick={() => banUser(user._id)}
-            disabled={user.role === "banned" || user.role === "admin"}
-          >
-            <span>Забанити</span>
-          </Button>
-        </div>
-      </ManageUserContainer>
+      {user ? (
+        <>
+          <h5>{user.name}</h5>
+          <ManageUserContainer>
+            <span className="user-data">
+              {user.email}
+              <br />
+              Роль: {user.role} <br />
+              ID: {user._id}
+            </span>
+            <div>
+              <Button
+                danger
+                onClick={() => banUser(user._id)}
+                disabled={user.role === "banned" || user.role === "admin"}
+              >
+                <span>Забанити</span>
+              </Button>
+            </div>
+          </ManageUserContainer>
+        </>
+      ) : (
+        <h5>Користувача не існує</h5>
+      )}
     </UserDisplayWrapper>
   );
 };
