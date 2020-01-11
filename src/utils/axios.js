4924ea29674c56f62b59bc75bd62e57c83c9746b
yaromyrshmid <yaromyr.shmid@gmail.com>
2020-01-11 13:01:35 +0200

    Switched back to heroku

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -3,9 +3,8 @@ import axios from "axios";
 const instance = axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
-      ? "lviv-bez-reklamy-server.netlify.com"
-      : // "https://lviv-bez-reklamy-server.herokuapp.com/"
-        "http://localhost:5000/"
+      ? "https://lviv-bez-reklamy-server.herokuapp.com/"
+      : "http://localhost:5000/"
 });
 
 export default instance;
